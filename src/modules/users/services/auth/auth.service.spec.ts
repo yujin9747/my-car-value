d078067618ddbde56952adaa26b6c506b32a51af
jang-yujin <jyj9747@naver.com>
2024-04-06 14:34:14 +0900

    feat: Test Step 2 - Getting Typescript to Help with Mocks

--- src/modules/users/services/auth/auth.service.spec.ts
+++ src/modules/users/services/auth/auth.service.spec.ts
@@ -2,17 +2,18 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "../users.service";
 import { HashPasswordService } from "../hash-password/hash-password.service";
+import { User } from "../../entities/user.entity";
 
 it('can create an instance of auth service', async () => {
-  const fakeUserService = {
-    // find: () => Promise.resolve([]),
-    // create: (email: string, password: string) =>
-    //   Promise.resolve({ id: 1, email, password }),
+  const fakeUserService: Partial<UsersService> = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
-  const fakeHashPasswordService = {
-    // hashPassword: () => Promise.resolve('salt.hash'),
-    // hashPasswordWithSalt: () => Promise.resolve('salt.hash'),
+  const fakeHashPasswordService: Partial<HashPasswordService> = {
+    hashPassword: () => Promise.resolve('salt.hash'),
+    createHashWithSalt: () => Promise.resolve('salt.hash'),
   };
 
   const module = await Test.createTestingModule({
